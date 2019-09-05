6a342a0127c3da64fcba8489d873138d23356ab1
Aaron <kongyajie0704@gmail.com>
2019-09-05 21:16:46 +0800

    update:修改配置

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var CONFIG = {
   // 网页 title
-  title: 'Book-Cli',
+  title: '前端技术梳理',
   
   // 网页 keywords
   keywords: 'book, book-cli, doc, example',
 
   // 网页 description
-  description: 'Init html book with markdown files.',
+  description: '前端技术梳理',
 
   // 链接是否打开新的窗口
   openNewWindow: true,
 
   // 页面右上角的 github 链接
-  github: 'https://github.com/vvpvvp/book-cli',
+  github: 'https://github.com/LLBTeam/Front-End-Book-At-Hand',
 
   // 左侧目录的md文件
   summaryMd: 'SUMMARY.md',
